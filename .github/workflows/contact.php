d2cdc47d6370b60931ea4226724c995dae44d2ec
sktrals01 <sktrails01@gmail.com>
2024-11-03 05:03:51 +0600

    Delete .github/workflows directory

--- .github/workflows/contact.php
+++
@@ -1,6 +0,0 @@
-
-<center>
-<link href="form_style.css" rel="stylesheet" type="text/css" />
-<? /* <iframe src="form.php" style="background-color:#eef;" align="top" frameborder="0" height="400" width="600" scrolling="no"></iframe> */ ?>
-<?php include('form.php'); ?>
-</center>
--- .github/workflows/dbconnect.inc.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$db_host = "localhost";
-$db_user = "megapers_3b";
-$db_pass = "yOmb+&]uqA;r";
-$db_name = "megapers_3b";
-?>
